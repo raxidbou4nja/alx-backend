ab6f0c4e5ac5239f26d0c6ff1e1b74175ab9d17d
raxidbou4nja <rachid.boughnja@gmail.com>
2024-05-08 21:04:11 +0100

    feat: Implement LFU cache algorithm in LFUCache class

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,38 @@
+# Create a class LFUCache that inherits from BaseCaching and is a caching system:
+#!/usr/bin/python3
+""" LFUCache module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """ LFUCache inherits from BaseCaching and is a caching system
+    """
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if key in self.queue:
+            self.queue.remove(key)
+        self.queue.append(key)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first = self.queue.pop(0)
+            del self.cache_data[first]
+            print("DISCARD: {}".format(first))
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        self.queue.remove(key)
+        self.queue.append(key)
+        return self.cache_data[key]
