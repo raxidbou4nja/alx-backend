6700ae1b46d9a8502844116610e5fdd87d065f30
raxidbou4nja <rachid.boughnja@gmail.com>
2024-05-15 00:33:43 +0100

    chore: Add Flask app and index template for i18n support

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app
+"""
+
+from flask import Flask, render_template
+from flask_babel import babel
+
+
+app = Flask(__name__)
+
+
+class Config(object):
+    """
+    Config class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Renders a basic html template
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
