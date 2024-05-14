99c82f3a6cf4d267ae65e48cf902c3c06a566538
raxidbou4nja <rachid.boughnja@gmail.com>
2024-05-15 00:50:35 +0100

    chore: Update Flask app to use Flask-Babel for i18n support

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -4,6 +4,7 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
 app = Flask(__name__)
 babel = Babel(app)
 
@@ -34,3 +35,4 @@ def index() -> str:
 
 
 if __name__ == "__main__":
+    app.run()
