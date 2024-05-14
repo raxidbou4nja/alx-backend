848df0f9ca73af904e2bb1505ef1961ed60b1361
raxidbou4nja <rachid.boughnja@gmail.com>
2024-05-15 00:20:26 +0100

    chore: Update index route and template in Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,12 +8,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    """ Index
+@app.route('/', strict_slashes=False)
+def index() -> str:
     """
-    return render_template('index.html')
+    Renders a basic html template
+    """
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
