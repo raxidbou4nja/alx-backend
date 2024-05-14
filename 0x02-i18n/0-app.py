3abfc3e3bc7d3e0395e9530539ac07d5fc000cde
raxidbou4nja <rachid.boughnja@gmail.com>
2024-05-15 00:03:59 +0100

    chore: Update Flask app and index template

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,4 +1,3 @@
-# First you will setup a basic Flask app in 0-app.py. Create a single / route and an index.html template that simply outputs “Welcome to Holberton” as page title (<title>) and “Hello world” as header (<h1>).
 #!/usr/bin/python3
 """
 Basic Flask app
