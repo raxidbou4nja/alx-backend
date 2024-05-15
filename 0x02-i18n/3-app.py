92bb474cec774e2cff04b5ceb91ea1f1e85319ee
raxidbou4nja <rachid.boughnja@gmail.com>
2024-05-15 01:06:59 +0100

    chore: Add index template for i18n support and update Flask app configuration

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" Module of index views
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ Config class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ Get locale
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """ GET /
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
