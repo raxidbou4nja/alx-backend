817c2bf4eea042a29978e0629e08a3b2e8ba259e
raxidbou4nja <rachid.boughnja@gmail.com>
2024-05-15 01:25:17 +0100

    chore: Update get_locale function to strip whitespace from locale argument

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,41 +1,48 @@
 #!/usr/bin/env python3
-""" Module of index views
 """
-
-from flask import Flask, render_template, request
+A Basic flask application
+"""
+from flask import Flask
+from flask import request
+from flask import render_template
 from flask_babel import Babel
 
 
+class Config(object):
+    """
+    Application configuration class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# Instantiate the application object
 app = Flask(__name__)
+app.config.from_object(Config)
+
+# Wrap the application with Babel
 babel = Babel(app)
 
 
-class Config:
-    """ Config class
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app.config.from_object(Config)
-
-
 @babel.localeselector
-def get_locale():
-    """ Get locale """
+def get_locale() -> str:
+    """
+    Gets locale from request object
+    """
     locale = request.args.get('locale', '').strip()
-    if locale in app.config['LANGUAGES']:
+    if locale and locale in Config.LANGUAGES:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def index() -> str:
-    """ GET /
+    """
+    Renders a basic html template
     """
     return render_template('4-index.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
